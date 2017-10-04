11bcd9ebc169ceeb82693ac8e30e0a3e93bb686c
Dieter Vercammen <dietervercammen@MacBook-Pro-van-Dieter.local>
2017-10-04 20:29:44 +0200

    models en migrations added

---
+++ Informaat/database/migrations/2017_10_04_133543_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('topic');
+            $table->text('body');
+            $table->string('tag1')->nullable();
+            $table->string('tag2')->nullable();
+            $table->string('tag3')->nullable();
+            $table->integer('upvotes')->nullable();
+            $table->integer('downvotes')->nullable();
+            $table->boolean('question')->default(true);
+            $table->boolean('upload')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
