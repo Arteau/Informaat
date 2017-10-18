6bd1e862ce3623a7acd0e5fcf0f7c7c93da0b16a
Dieter Vercammen <dietervercammen@MacBook-Pro-van-Dieter.local>
2017-10-18 20:53:49 +0200

    search page css added

--- Informaat/resources/views/post/search.blade.php
+++ Informaat/resources/views/post/search.blade.php
@@ -19,35 +19,41 @@
 
         @foreach($searchPost as $post)
         
-         <!-- Blog Post -->
-         <div class="blog_post md-4 alert alert-info">
-            <div class="card ">
-                <div class="card-body">
-                
+        <div class="card horizontal z-depth-2 hoverable">
+        <div class="card-image">
+          <img src="https://maxcdn.icons8.com/Share/icon/color/Gaming//pokecoin1600.png" style="width:60%">
+        </div>
+        <div class="card-stacked">
+          <div class="card-content">
                 {{ $post->votes }}
-                <!-- @if(!$user->hasVoted($post))
-                <a href="/posts/{{$post->id}}/upvote"><i class="fa fa-caret-up" aria-hidden="true"></i></a>
-                <a href="/posts/{{$post->id}}/downvote"><i class="fa fa-caret-down" aria-hidden="true"></i></a>
-                @else
-                    @if($user->hasUpVoted($post))
-                        <a href="/posts/{{$post->id}}/cancelvote"><i class="fa fa-caret-up" aria-hidden="true" style="color:black"></i></a>
-                        <a href="/posts/{{$post->id}}/downvote"><i class="fa fa-caret-down" aria-hidden="true"></i></a>
-                    @endif
-                    @if($user->hasDownVoted($post))
-                        <a href="/posts/{{$post->id}}/upvote"><i class="fa fa-caret-up" aria-hidden="true"></i></a>
-                        <a href="/posts/{{$post->id}}/cancelvote"><i class="fa fa-caret-down" aria-hidden="true" style="color:black"></i></a>
-                    @endif
-                @endif -->
-                <!-- return back geeft geen posts meer mee in compatct!!!!!!!!!! -->
+                  @if(!$user->hasVoted($post))
+                  <a href="/posts/{{$post->id}}/upvote"><i class="fa fa-caret-up" aria-hidden="true"></i></a>
+                  <a href="/posts/{{$post->id}}/downvote"><i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                  @else
+                      @if($user->hasUpVoted($post))
+                          <a href="/posts/{{$post->id}}/cancelvote"><i class="fa fa-caret-up" aria-hidden="true" style="color:black"></i></a>
+                          <a href="/posts/{{$post->id}}/downvote"><i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                      @endif
+                      @if($user->hasDownVoted($post))
+                          <a href="/posts/{{$post->id}}/upvote"><i class="fa fa-caret-up" aria-hidden="true"></i></a>
+                          <a href="/posts/{{$post->id}}/cancelvote"><i class="fa fa-caret-down" aria-hidden="true" style="color:black"></i></a>
+                      @endif
+                  @endif
 
-                <h2 class="card-title">{{$post->title}}</h2>
-                <i>{{$post->body}}</i>
-                <hr>
-                <a href="/posts/{{$post->id}}" class="btn btn-primary">Read More &rarr;</a>
-                <a href="/posts/{{$post->id}}/edit"><button class="btn btn-default">edit post</button></a>
-                </div>
-            </div>
+                  <h2 class="card-title">{{$post->title}}</h2>
+                  <i>{{$post->body}}</i>
+                  <hr>
+                  
+                  <p>Onderwerp: {{$post->topic}}</p> 
+                  @if(!empty($post->tag1 | $post->tag2 | $post->tag3))
+                  <small>{{$post->tag1}} // {{$post->tag2}} // {{$post->tag3}}</small>
+                  @endif
           </div>
+          <div class="card-action">
+            <a href="/posts/{{$post->id}}">Lees meer</a>
+          </div>
+        </div>
+      </div>
 
         @endforeach
 
--- Informaat/resources/views/welcome.blade.php
+++ Informaat/resources/views/welcome.blade.php
@@ -5,15 +5,21 @@
 
 <div class="blue-grey lighten-4 landing_page">
 
-<div class="row">
+    <div class="row">
+        <div class="col s12 blue-grey lighten-1">
+            <h1 class="center-align flow-text black-text">Welkom bij informaat</h1> 
+        </div>
+    </div>
+    <div class="row">
+    <div class="col m8 offset-m2 s12">
+        <form role="search" class="app-search" action="/posts/search" method="POST">
+            {{ csrf_field() }}
 
-<div class="col s12 blue-grey lighten-1">
-
-<h1 class="center-align flow-text black-text">Welkom bij informaat</h1> 
-
-</div>
-
-</div>
+            @include ('layouts.errors')
+            <label for="keyword">Zoeken</label>
+            <input type="text" name="keyword" id="keyword"  class="field-input" style="width:100%"> <a href=""></a>
+        </form>
+    </div>
 
 <div class="row">
 
